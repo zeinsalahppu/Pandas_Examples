551a1fb3dc1d0392336684fa37518ef5c4f33b39
zeinsalahppu <zeinsalah@gmail.com>
2020-04-25 12:12:51 +0300

    dataframe from dictionary

--- Pasndas_examples.py
+++ Pasndas_examples.py
@@ -6,3 +6,35 @@ Semester: SS 2020
 
 import pandas as pd
 
+# passengers = pd.DataFrame(
+#     {
+#     "Id": [111, 222, 333, 444, 555],
+#     "Name": ["Adam", "Bernhard", "Christina", "Dirk", "Elizabeth"],
+#     "Age": [18, 30, 25, 55, 39],
+#     "Gender": ["male", "male", "female", "male", "female"]
+#     }
+# )
+#
+# print(passengers)
+
+psg_dict = {
+    "Id": [111, 222, 333, 444, 555],
+    "Name": ["Adam", "Bernhard", "Adam", "Dirk", "Elizabeth"],
+    "Age": [18, 30, 25, 55, 39],
+    "Gender": ["male", "male", "female", "male", "female"]
+    }
+
+passengers_df = pd.DataFrame(psg_dict)
+# # print(passengers_df)
+# # print(type(passengers_df))
+#
+# names = passengers_df["Name"]
+# print(names)
+# # print(type(names))
+#
+# ages = passengers_df["Age"]
+# print(ages)
+# print(ages.max())
+# print(ages.mean())
+
+print(passengers_df.first_valid_index)
