7ad49cc4cfd6a5691c9764b4f12ee688b5556b09
zeinsalahppu <zeinsalah@gmail.com>
2020-04-25 23:32:04 +0300

    selecting rows and columns loc and iloc

--- Pasndas_examples.py
+++ Pasndas_examples.py
@@ -6,3 +6,35 @@ Semester: SS 2020
 
 import pandas as pd
 
+soccer_df = pd.read_csv("data/england-premier-league-players-2018-to-2019-stats.csv")
+#print(soccer_df.info())
+
+
+# pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', 6)
+# pd.set_option('display.width', None)
+# #pd.set_option('display.max_colwidth', -1)
+
+#print(soccer_df)
+
+compact_soccer = soccer_df[["full_name", "age", "nationality", "Current Club", "position"]]
+# print(compact_soccer)
+# compact_soccer = compact_soccer.set_index('full_name')
+
+# ms = compact_soccer.loc["Mohamed Salah"]
+# print(ms)
+#
+# liverpool_df = compact_soccer.loc[compact_soccer["Current Club"] == "Liverpool"]
+# print(liverpool_df)
+#
+# soccer_over37 = compact_soccer.loc[compact_soccer["age"] > 37]
+# print(soccer_over37)
+
+p1 = compact_soccer.iloc[0]
+print(p1)
+
+smaller_soccer1_df = compact_soccer.iloc[0:40:5]
+print(smaller_soccer1_df)
+
+smaller_soccer2_df = compact_soccer.iloc[0:40:5, :3]
+print(smaller_soccer2_df)
