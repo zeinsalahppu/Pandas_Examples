38dba7e5c2afb6087401d833a2914694eab60d55
zeinsalahppu <zeinsalah@gmail.com>
2020-04-26 22:56:54 +0300

    plot aggregated data

--- Pasndas_examples.py
+++ Pasndas_examples.py
@@ -5,18 +5,33 @@ Semester: SS 2020
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 flight_df = pd.read_csv("data/raw-flight-data.csv")
-print(flight_df)
+# print(flight_df)
 
-print(flight_df[["DepDelay", "ArrDelay"]].mean())
+# print(flight_df[["DepDelay", "ArrDelay"]].mean())
+#
+# gbo1 = flight_df.groupby("Carrier")
+# print(type(gbo1))
+# print(gbo1.mean())
+# print(gbo1[["DepDelay", "ArrDelay"]].mean())
+#
+# gbo2 = flight_df[["Carrier", "DepDelay", "ArrDelay"]].groupby("Carrier")
+# print(gbo2.mean())
+# print(gbo2.describe())
 
-gbo1 = flight_df.groupby("Carrier")
-print(type(gbo1))
-print(gbo1.mean())
-print(gbo1[["DepDelay", "ArrDelay"]].mean())
+gbo3 = flight_df.groupby("OriginAirportID")
 
-gbo2 = flight_df[["Carrier", "DepDelay", "ArrDelay"]].groupby("Carrier")
-print(gbo2.mean())
-print(gbo2.describe())
+print(gbo3[["DepDelay", "ArrDelay"]].sum())
 
+print(gbo3[["DepDelay", "ArrDelay"]].agg(lambda x: x[x>0].sum()))
+
+y = gbo3["DepDelay"].agg(lambda x: x[x>0].sum())
+x = y.index
+
+fig, ax = plt.subplots()
+ax.bar(x, y, 100)
+ax.set_yticks( list(range(0, 2000000, 500000)) )
+plt.show()
