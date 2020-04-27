7705ecfd2c2e79952caca803ae272839961f9a1b
zeinsalahppu <zeinsalah@gmail.com>
2020-04-27 17:57:46 +0300

    another solution using scipy

--- Pasndas_examples.py
+++ Pasndas_examples.py
@@ -7,25 +7,29 @@ Semester: SS 2020
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, linear_model
+from scipy import stats
+
 
 ice_df = pd.read_csv("data/ics_vs_temp.csv")
 
 temp_list = ice_df["AvgTemperature"]
-temp_list = temp_list[:, np.newaxis]
 ice_sales_list = ice_df["IcecreamSales"]
 
-regr = linear_model.LinearRegression()
+slope, intercept, r, p, std_err = stats.linregress(temp_list, ice_sales_list) # x, y
 
-regr.fit(temp_list, ice_sales_list)
-
-predicted_ice_sales = regr.predict(temp_list)
+def line_funtion(x):
+  return slope * x + intercept
 
 test_data = np.array([30, 40])
-test_data = test_data[:, np.newaxis]
+print(line_funtion(test_data))
 
-print(regr.predict(test_data))
+print(r)
 
+y = line_funtion(temp_list)
 plt.scatter(temp_list, ice_sales_list)
-plt.plot(temp_list, predicted_ice_sales)
+plt.plot(temp_list, y)
+
 plt.show()
+
+
+
